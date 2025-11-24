dc077526ddd90a84bae0b46d765e6f0938b69746
neritinc <szu.pet99@gmai.com>
2025-11-24 12:04:24 +0100

    Schoolclass fileok készítés

---
+++ server/app/Http/Controllers/SchoolclassController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Schoolclass;
+use App\Http\Requests\StoreSchoolclassRequest;
+use App\Http\Requests\UpdateSchoolclassRequest;
+
+class SchoolclassController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreSchoolclassRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Schoolclass $schoolclass)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateSchoolclassRequest $request, Schoolclass $schoolclass)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Schoolclass $schoolclass)
+    {
+        //
+    }
+}
