41845a926bc121305b1b6e0b7f1584d131ef3266
neritinc <szu.pet99@gmai.com>
2025-11-24 12:01:04 +0100

    Shcoolclass elrontva

---
+++ server/app/Http/Controllers/ShcoolclassController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Shcoolclass;
+use App\Http\Requests\StoreShcoolclassRequest;
+use App\Http\Requests\UpdateShcoolclassRequest;
+
+class ShcoolclassController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreShcoolclassRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Shcoolclass $shcoolclass)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateShcoolclassRequest $request, Shcoolclass $shcoolclass)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Shcoolclass $shcoolclass)
+    {
+        //
+    }
+}
