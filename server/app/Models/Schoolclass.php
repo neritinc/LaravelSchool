0ac415d3972c1082e9534baa20843fc054ebd09a
neritinc <szu.pet99@gmai.com>
2025-11-24 12:44:06 +0100

    server/app/models Sport-Student generálás

--- server/app/Models/Schoolclass.php
+++ server/app/Models/Schoolclass.php
@@ -5,8 +5,15 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Schoolclass extends Model
+class SchoolClass extends Model
 {
-    /** @use HasFactory<\Database\Factories\SchoolclassFactory> */
     use HasFactory;
+
+    protected $table = 'schoolclasses'; // Biztos ami biztos
+
+    // Egy osztálynak sok diákja van
+    public function students()
+    {
+        return $this->hasMany(Student::class, 'schoolclassId', 'id');
+    }
 }
